641b89ff2e71d3e15da63a2531015a54ae77fd13
Andy Pearce <andy.pearce@austrocontrol.at>
2016-04-26 16:31:25 +0200

    Tidy up files

--- admin/models/expense.php
+++ admin/models/expense.php
@@ -6,146 +6,126 @@
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
- 
+
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
- 
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
+
 /**
  * Bank Model
  *
- * @since  0.0.1
+ * @since 0.0.1
  */
-class BankModelExpense extends JModelAdmin
-{
-	
+class BankModelExpense extends JModelAdmin {
 	var $_transIndex = 1;
-	
-	  function __construct()
-	  {
-	 	parent::__construct();
-	 
-	  	$mainframe = JFactory::getApplication();
-	 
-		// Should be recieve with the view class.
-		$acc_id = $mainframe->getUserStateFromRequest('trans_id', 'trans_id', 0, 'int');
-		$this->setState('trans_id', $acc_id);
+	function __construct() {
+		parent::__construct ();
 		
-	  }
-  
+		$mainframe = JFactory::getApplication ();
+		
+		// Should be recieve with the view class.
+		$acc_id = $mainframe->getUserStateFromRequest ( 'trans_id', 'trans_id', 0, 'int' );
+		$this->setState ( 'trans_id', $acc_id );
+	}
+	
 	/**
 	 * Method to get a table object, load it if necessary.
 	 *
-	 * @param   string  $type    The table name. Optional.
-	 * @param   string  $prefix  The class prefix. Optional.
-	 * @param   array   $config  Configuration array for model. Optional.
-	 *
-	 * @return  JTable  A JTable object
-	 *
-	 * @since   1.6
+	 * @param string $type
+	 *        	The table name. Optional.
+	 * @param string $prefix
+	 *        	The class prefix. Optional.
+	 * @param array $config
+	 *        	Configuration array for model. Optional.
+	 *        	
+	 * @return JTable A JTable object
+	 *        
+	 * @since 1.6
 	 */
-	public function getTable($type = 'Expense', $prefix = 'ExpenseTable', $config = array())
-	{
-		return JTable::getInstance($type, $prefix, $config);
+	public function getTable($type = 'Expense', $prefix = 'ExpenseTable', $config = array()) {
+		return JTable::getInstance ( $type, $prefix, $config );
 	}
- 
+	
 	/**
 	 * Method to get the record form.
 	 *
-	 * @param   array    $data      Data for the form.
-	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
-	 *
-	 * @return  mixed    A JForm object on success, false on failure
-	 *
-	 * @since   1.6
+	 * @param array $data
+	 *        	Data for the form.
+	 * @param boolean $loadData
+	 *        	True if the form is to load its own data (default case), false if not.
+	 *        	
+	 * @return mixed A JForm object on success, false on failure
+	 *        
+	 * @since 1.6
 	 */
-	public function getForm($data = array(), $loadData = true)
-	{
+	public function getForm($data = array(), $loadData = true) {
 		// Get the form.
-		dump($this,"ExpenseForm - getForm");
+		dump ( $this, "ExpenseForm - getForm" );
 		
-		$form = $this->loadForm(
-			'com_bank.expense',
-			'expense',
-			array(
+		$form = $this->loadForm ( 'com_bank.expense', 'expense', array (
 				'control' => 'jform',
-				'load_data' => $loadData
-			)
-		);
- 
-		if (empty($form))
-		{
+				'load_data' => $loadData 
+		) );
+		
+		if (empty ( $form )) {
 			return false;
 		}
- 
+		
 		return $form;
 	}
- 
+	
 	/**
 	 * Method to get the data that should be injected in the form.
 	 *
-	 * @return  mixed  The data for the form.
-	 *
-	 * @since   1.6
+	 * @return mixed The data for the form.
+	 *        
+	 * @since 1.6
 	 */
-	protected function loadFormData()
-	{
+	protected function loadFormData() {
 		// Check the session for previously entered form data.
-		dump($this,"ExpenseForm - loadFormData");
-		$data = JFactory::getApplication()->getUserState(
-			'com_bank.expense.data',
-			array()
-		);
- 
-		dump($this,"ExpenseForm - loadFormData 1");
-		if (empty($data))
-		{
+		dump ( $this, "ExpenseForm - loadFormData" );
+		
+		$data = JFactory::getApplication ()->getUserState ( 'com_bank.expense.data', array () );
+		
+		if (empty ( $data )) {
 			
-			// Get the data for the form.
-		dump($this,"ExpenseForm - loadFormData 2 before");
-			//$myTable = getTable();
-		
-		
-		// Initialize variables.
-		$db    = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		
-		$trans_id = JFactory::getApplication()->input->get('trans_id');
-		
-		if (trans_id == null)
-		{
-			$trans_id = $this->getState('trans_id');
+			// Initialize variables.
+			$db = JFactory::getDbo ();
+			$query = $db->getQuery ( true );
+			
+			$trans_id = JFactory::getApplication ()->input->get ( 'trans_id' );
+			
+			if (trans_id == null) {
+				$trans_id = $this->getState ( 'trans_id' );
+			}
+			
+			// Create the base select statement.
+			$query->select ( '*' )->where ( $db->quoteName ( 'trans_id' ) . " = " . $db->quote ( $trans_id ) )->from ( $db->quoteName ( '#__bank_expense' ) );
+			
+			$db->setQuery ( $query );
+			$row = $db->loadAssoc ();
+			
+			// Check that we have a result.
+			if (empty ( $row )) {
+				$data = false;
+			} else {
+				// Bind the object with the row and return.
+				$data = $this->getTable ();
+				$res = $data->bind ( $row );
+			}
 		}
+
 		
-		// Create the base select statement.
-		$query->select('*')
-		->where($db->quoteName('trans_id')." = ".$db->quote($trans_id))
-		->from($db->quoteName('#__bank_expense'));
+		$mainframe = JFactory::getApplication();
+		$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', 
+							$mainframe->getCfg('list_limit'), 'int');
+		$limitstart1 = $mainframe->getUserStateFromRequest ( 'limitstart', 'limitstart', 0, 'int' );
+		$limitstart2 = $this->getState('limitstart');
 		
-		dump($this,"ExpenseForm - loadFormData a1");
-		$db->setQuery($query);
-		dump($this,"ExpenseForm - loadFormData a2");
+		dump($this,"Check pag status..");
+		dump($limitstart1,"getData limitstart1");
+		dump($limitstart2,"getData limitstart2");
+		dump($limit,"getData limit");
 		
-		$row = $db->loadAssoc();
-		dump($this,"ExpenseForm - loadFormData a3");
-		
-		// Check that we have a result.
-		if (empty($row))
-		{
-		dump($this,"ExpenseForm - loadFormData a4");
-			$data = false;
-		}
-		else
-		{
-		dump($this,"ExpenseForm - loadFormData a5");
-			// Bind the object with the row and return.
-			$data=$this->getTable();
-		dump($table,"ExpenseForm - loadFormData 5a");
-			$res = $data->bind($row);
-		}
-		dump($data,"ExpenseForm - loadFormData a6");
-		dump($this,"ExpenseForm - loadFormData a6");
-		}
-		dump($this,"ExpenseForm - loadFormData 3");
 		
 		return $data;
 	}
--- admin/views/expense/view.html.php
+++ admin/views/expense/view.html.php
@@ -6,85 +6,74 @@
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
- 
+
 // No direct access to this file
-defined('_JEXEC') or die('Restricted access');
- 
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
+
 /**
  * Bank View
  *
- * @since  0.0.1
+ * @since 0.0.1
  */
-class BankViewExpense extends JViewLegacy
-{
+class BankViewExpense extends JViewLegacy {
 	/**
 	 * View form
 	 *
-	 * @var         form
+	 * @var form
 	 */
 	protected $form = null;
- 
+	
 	/**
 	 * Display the Bank view
 	 *
-	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
-	 *
-	 * @return  void
+	 * @param string $tpl
+	 *        	The name of the template file to parse; automatically searches through the template paths.
+	 *        	
+	 * @return void
 	 */
-	public function display($tpl = null)
-	{
+	public function display($tpl = null) {
 		// Get the Data
-		$this->form = $this->get('Form');
- 
-		dump($this->getName(), 'In expense View');
-		dump($this, 'In expense View');
+		$this->form = $this->get ( 'Form' );
 		
 		// Check for errors.
-		if (count($errors = $this->get('Errors')))
-		{
-			JError::raiseError(500, implode('<br />', $errors));
- 
+		if (count ( $errors = $this->get ( 'Errors' ) )) {
+			JError::raiseError ( 500, implode ( '<br />', $errors ) );
+			
 			return false;
 		}
- 
- 
+		
 		// Set the toolbar
-		$this->addToolBar();
- 
+		$this->addToolBar ();
+		
 		// Display the template
-		parent::display($tpl);
+		parent::display ( $tpl );
 	}
- 
+	
 	/**
 	 * Add the page title and toolbar.
 	 *
-	 * @return  void
+	 * @return void
 	 *
-	 * @since   1.6
+	 * @since 1.6
 	 */
-	protected function addToolBar()
-	{
-		$input = JFactory::getApplication()->input;
- 
+	protected function addToolBar() {
+		$input = JFactory::getApplication ()->input;
+		
 		// Hide Joomla Administrator Main menu
-		$input->set('hidemainmenu', false);
- 
+		$input->set ( 'hidemainmenu', false );
+		
+		dump($this,"addToolBar - start");
+		
 		$isNew = ($this->item->id == 0);
- 
-		if ($isNew)
-		{
-			$title = JText::_('COM_BANK_EXPENSE_NEW');
+		
+		if ($isNew) {
+			$title = JText::_ ( 'COM_BANK_EXPENSE_NEW' );
+		} else {
+			$title = JText::_ ( 'COM_BANK_EXPENSE_EDIT' );
 		}
-		else
-		{
-			$title = JText::_('COM_BANK_EXPENSE_EDIT');
-		}
- 
-		JToolBarHelper::title($title, 'expense');
-		JToolBarHelper::save('bank.expense');
-		JToolBarHelper::cancel(
-			'expense.cancel',
-			$isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE'
-		);
+		
+		JToolBarHelper::title ( $title, 'expense' );
+		JToolBarHelper::save ( 'accentries.expenseList' );
+		JToolBarHelper::cancel ( 'accentries.expenseList', $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE' );
 	}
 }
